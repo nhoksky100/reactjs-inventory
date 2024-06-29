9be500c8c251fb120ec32fc6d834a938325648d5
nhoksky100 <65888688+nhoksky100@users.noreply.github.com>
2024-06-29 14:53:10 +0700

    RequestHistoryExport.jsx

--- src/Component/Header/ProfileHistory/RequestHistoryExport.jsx
+++ src/Component/Header/ProfileHistory/RequestHistoryExport.jsx
@@ -277,7 +277,7 @@ class RequestHistoryExport extends Component {
         if (this._isMounted) {
             const filteredData = sortedData.filter(value => {
 
-                const pointApprove = value.requestTransferPointApprove !== null ? value.requestTransferPointApprove.split(',') : '';
+                const pointApprove = typeof value.requestTransferPointApprove==='string' && value.requestTransferPointApprove !== null ? value.requestTransferPointApprove.split(',') : '';
                 return !this.checkPermissionApprove(pointApprove, value)
             });
             this.props.getDataSearch(filteredData)
@@ -450,7 +450,7 @@ class RequestHistoryExport extends Component {
                 dataRequestTransferHistory.forEach((history) => {
                     if (history.idHistory === requestIdHistory) {
                         // Lấy idRequestTransfers từ phần tử history và kiểm tra idRequest
-                        const idHistorys = history.idRequestTransfers !== null ? history.idRequestTransfers.split(',') : [];
+                        const idHistorys = typeof history.idRequestTransfers==='string' && history.idRequestTransfers !== null ? history.idRequestTransfers.split(',') : [];
 
                         if (idHistorys.includes(idRequest)) {
                             matchedIdHistory = history.idHistory;
@@ -788,8 +788,8 @@ class RequestHistoryExport extends Component {
                 currentTodos = this.currentTodos(dataRequest)
             }
             return currentTodos.map((value, key) => {
-                const approveted = value.requestTransferApprove !== null ? value.requestTransferApprove.split(',') : ''
-                const pointApprove = value.requestTransferPointApprove !== null ? value.requestTransferPointApprove.split(',') : ''
+                const approveted = typeof  value.requestTransferApprove==='string' && value.requestTransferApprove !== null ? value.requestTransferApprove.split(',') : ''
+                const pointApprove = typeof  value.requestTransferPointApprove ==='string' &&  value.requestTransferPointApprove !== null ? value.requestTransferPointApprove.split(',') : ''
                 return (
                     <tr key={key} >
                         {value.requestTransferStatus === 'Đã duyệt' || value.requestTransferStatus === 'Từ chối' ?
